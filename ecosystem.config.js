35f573042071bff11ae2a788a816e0630769fa41
Dmitrii Kulikov <garf.freeman@gmail.com>
2017-06-02 15:49:25 +0300

    Fix error with json result

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,11 @@ module.exports = {
     {
       name      : 'se-proxy-app',
       script    : 'index.js',
-      max_memory_restart: '768M'
+      max_memory_restart: '768M',
+      env: {
+        "NODE_ENV": "production",
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
     }
   ]
 };
